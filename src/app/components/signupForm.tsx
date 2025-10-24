992b2d4652e9d60a8f3c2d982ea5de6267a454a3
LoanDervillers <loan.dervillers.developpement@gmail.com>
2025-10-24 09:25:46 +0200

    Refonte évènement

--- src/app/components/signupForm.tsx
+++ src/app/components/signupForm.tsx
@@ -2,7 +2,8 @@
 
 import { useState } from "react";
 import { supabase } from "../lib/supabaseClient";
-import DynamicSelectGroup from "./DynamicSelectGroup";
+//import DynamicSelectGroup from "./DynamicSelectGroup";
+//import { EventFormData } from "@/types/EventFormData";
 
 type Role = "residente" | "invitee";
 
@@ -167,11 +168,11 @@ export default function SignupForm({ role, onSubmit }: Props) {
                         className="w-full mb-3 px-4 py-2 border border-blue-500 text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         required
                     />
-                    {/* Résidence, étage et chambre */}
+                    {/* Résidence, étage et chambre
                     <DynamicSelectGroup
-                        category="residence"
+                        category="Résidence"
                         onChange={(values) => setSelection(values)}
-                    />
+                    /> */}
 
                 </> 
             )}
